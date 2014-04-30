a5ce070e09624879771bd62db938a106f42c7f28
Kris Unnikannan <krish.unnikannan@gmail.com>
2014-04-29 21:35:57 -0500

    Clean up and daily commit

--- public/script.js
+++ public/script.js
@@ -1,54 +1,40 @@
 /*Contoller for STY*/
 var STY = angular.module('STY', []);
 
-STY.config(function($httpProvider) {
-    //Enable cross domain calls
-    $httpProvider.defaults.useXDomain = true;
-    //Remove the header used to identify ajax call  that would prevent CORS from working
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
-/* $scope.url = 7WNdMwM_oFQ';*/
 
-
-function Systems($scope) {
-    $scope.system = 'ouya';
-    $scope.system = ["ouya", "beta", "mac"];
-
-}
-
-var SystemsCtrl = function($scope) {
-
-    $scope.singleModel = 1;
-
-    $scope.radioModel = 'ouya';
-
-    $scope.checkModel = {
-        left: false,
-        middle: true,
-        right: false
-    };
-};
-
-STY.controller('STYCtrl', function($scope, $http) {
-    $scope.SendURL = function() {
-        /*        var YoutubePayload = '?request={"jsonrpc": "2.0", "method": "Player.Open", "params":{"item": {"file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + $scope.url + '" }}, "id" : "1"}';
-         */
-        var YoutubePayload = '{"jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "plugin://plugin.video.youtube/?action=play_video&videoid=nC1yLD7R0ZU" }  }, "id": "libPlayerOpen"}'
-        $http.defaults.useXDomain = true;
-        $http({
-            method: 'POST',
-            url: 'http://pi.ku:5000/SendProxy',
-            data: YoutubePayload,
-            headers: {
-                'Content-type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': ['OPTIONS', 'GET', 'POST'],
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Server':$scope.system
-            },
-        });
-
-        console.log($scope.url);
+    function Systems($scope) {
+        $scope.system = 'ouya.ku';
+        $scope.system = ["ouya.ku", "beta.ku", "mac"];
     }
 
-});
+    var SystemsCtrl = function($scope) {
+        $scope.singleModel = 1;
+        $scope.radioModel = 'ouya';
+        $scope.checkModel = {
+            left: false,
+            middle: true,
+            right: false
+        };
+    };
+
+    STY.controller('STYCtrl', function($scope, $http) {
+        $scope.SendURL = function() {
+            var YoutubePayload = '{"jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "plugin://plugin.video.youtube/?action=play_video&videoid=nC1yLD7R0ZU" }  }, "id": "libPlayerOpen"}'
+            $http.defaults.useXDomain = true;
+            $http({
+                method: 'POST',
+                url: 'http://pi.ku:5000/SendProxy',
+                data: YoutubePayload,
+                headers: {
+                    'Content-type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': ['OPTIONS', 'GET', 'POST'],
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'hostname': $scope.system
+                },
+            });
+            console.log($scope.system);
+            console.log($scope.url);
+        }
+
+    });
--- server.js
+++ server.js
@@ -2,14 +2,13 @@ var express = require('express');
 var app = express(),
     http = require('http'),
     url = require('url'),
-    Xbmc = require('xbmc'),
     server = http.createServer(app);
 
 app.use(express.compress());
 app.use(express.json());
 app.use(express.urlencoded());
-/*app.use(express.favicon());
- */
+app.use(express.favicon(__dirname + '/public/images/icon-youtube.ico'));
+
 app.use(express.urlencoded());
 server.listen(5000);
 app.use(express.static(__dirname + '/public'));
@@ -19,52 +18,31 @@ app.configure('development', function() {
         showStack: true
     }));
 });
-
-
-
-
-/*
-    var url_parts = url.parse(req.url, true);
-    var request = url_parts.query;
-    var xbmcApi = new Xbmc.XbmcApi;
-    xbmcApi.setConnection(connection);
-
-    res.send('<p> Command:' + request.command + ':Recieved</p>');
-
-    var id = process.argv[2] || 'QH2-TGUlwu4';
-
-    xbmcApi.player.openYoutube(id);
-*/
-
-
 app.post('/SendProxy', function(req, res) {
     var options = {
-        host: 'beta.ku',
+        host: req.headers.hostname,
         port: 8080,
         path: '/jsonrpc',
         method: 'POST'
     };
+    /*host is send as hostname in the header*/
+    console.log('host:' + (req.headers.hostname));
+    /* Test youtube Payload*/
     var YoutubePayload = '{"jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "plugin://plugin.video.youtube/?action=play_video&videoid=nC1yLD7R0ZU" }  }, "id": "libPlayerOpen"}'
 
-    var reqw = http.request(options, function(res) {
+    var ProxyRequest = http.request(options, function(res) {
         console.log('BODY:' + JSON.stringify(req.body));
-        console.log('SERVER:' + JSON.stringify(req.headers.server));
-
+        console.log('SERVER:' + JSON.stringify(req.headers.hostname));
 
         res.setEncoding('utf8');
         res.on(req.body, function(chunk) {
             console.log('BODY: ' + chunk);
         });
     });
-
-    reqw.on('error', function(e) {
+    ProxyRequest.on('error', function(e) {
         console.log('problem with request: ' + e.message);
     });
-
-    // write data to request body
-    /*        reqw.write(YoutubePayload);
-     */
-    reqw.write(JSON.stringify(req.body));
-    reqw.end();
+    ProxyRequest.write(JSON.stringify(req.body));
+    ProxyRequest.end();
     res.send('<p> Command:' + ':Recieved</p>');
 });
