9e5ca76913110463bf9618f9793453be7e9fb17b
Kris Unnikannan <krish.unnikannan@gmail.com>
2014-04-30 22:23:29 -0500

    cleaning up angular code

--- public/script.js
+++ public/script.js
@@ -1,39 +1,32 @@
 /*Contoller for STY*/
 var STY = angular.module('STY', []);
 
-
-    function Systems($scope) {
-        $scope.system = 'ouya.ku';
-        $scope.system = ["ouya.ku", "beta.ku", "mac"];
-    }
-
-    var SystemsCtrl = function($scope) {
-        $scope.singleModel = 1;
-        $scope.radioModel = 'ouya';
-        $scope.checkModel = {
-            left: false,
-            middle: true,
-            right: false
-        };
+STY.controller('HostSystem', function($scope, $http) 
+{   
+    /*$scope.user = {
+        system: "ouya.ku"
     };
-
-    STY.controller('STYCtrl', function($scope, $http) {
-        $scope.SendURL = function() {
-            var YoutubePayload = '{"jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "plugin://plugin.video.youtube/?action=play_video&videoid=nC1yLD7R0ZU" }  }, "id": "libPlayerOpen"}'
+    */
+    $scope.currentHost='ouya';
+    $scope.system = ["ouya", "beta", "mac"];
+    });
+STY.controller('STYCtrl', function($scope, $http) {
+        $scope.SendURL = function($host) {
+            var STYPayload = '{"jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "plugin://plugin.video.youtube/?action=play_video&videoid='+$scope.url+'" }  }, "id": "libPlayerOpen"}'
             $http.defaults.useXDomain = true;
             $http({
                 method: 'POST',
                 url: 'http://pi.ku:5000/SendProxy',
-                data: YoutubePayload,
+                data: STYPayload,
                 headers: {
                     'Content-type': 'application/json',
                     'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Methods': ['OPTIONS', 'GET', 'POST'],
                     'Access-Control-Allow-Headers': 'Content-Type',
-                    'hostname': $scope.system
+                    'hostname': $host+'.ku'
                 },
             });
-            console.log($scope.system);
+            console.log($host);
             console.log($scope.url);
         }
 
