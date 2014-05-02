49c073bb9e0d66899ffed4ee0890b6c85b011b41
Kris Unnikannan <krish.unnikannan@gmail.com>
2014-05-01 22:07:07 -0500

    Url pharsing

--- public/script.js
+++ public/script.js
@@ -1,33 +1,29 @@
 /*Contoller for STY*/
 var STY = angular.module('STY', []);
 
-STY.controller('HostSystem', function($scope, $http) 
-{   
-    /*$scope.user = {
-        system: "ouya.ku"
-    };
-    */
-    $scope.currentHost='ouya';
+STY.controller('HostSystem', function($scope, $http) {
+    $scope.currentHost = 'ouya';
     $scope.system = ["ouya", "beta", "mac"];
-    });
+});
 STY.controller('STYCtrl', function($scope, $http) {
-        $scope.SendURL = function($host) {
-            var STYPayload = '{"jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "plugin://plugin.video.youtube/?action=play_video&videoid='+$scope.url+'" }  }, "id": "libPlayerOpen"}'
-            $http.defaults.useXDomain = true;
-            $http({
-                method: 'POST',
-                url: 'http://pi.ku:5000/SendProxy',
-                data: STYPayload,
-                headers: {
-                    'Content-type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': ['OPTIONS', 'GET', 'POST'],
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                    'hostname': $host+'.ku'
-                },
-            });
-            console.log($host);
-            console.log($scope.url);
-        }
+    $scope.SendURL = function($host) {
+        var PrsdUrl = $scope.url.substr($scope.url.indexOf('=') + 1, $scope.url.length);
+        var STYPayload = '{"jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "plugin://plugin.video.youtube/?action=play_video&videoid=' + PrsdUrl + '" }  }, "id": "libPlayerOpen"}'
+        $http.defaults.useXDomain = true;
+        $http({
+            method: 'POST',
+            url: 'http://pi.ku:5000/SendProxy',
+            data: STYPayload,
+            headers: {
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': ['OPTIONS', 'GET', 'POST'],
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'hostname': $host + '.ku'
+            },
+        });
+        console.log($host);
+        console.log($scope.url);
+    }
 
-    });
+});
